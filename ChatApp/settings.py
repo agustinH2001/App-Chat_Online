72501ea3c5c9c5584ff151d0ff54ca5f8774f0b5
agustinH2001 <agustinlugo2019@gmail.com>
2022-09-29 09:56:40 -0300

    Update settings.py

--- ChatApp/settings.py
+++ ChatApp/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'chat.apps.ChatConfig',
     'django.contrib.admin',
     'django.contrib.auth',
+    'django.contrib.staticfiles',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
